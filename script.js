83ecfdeb2f2d9b3558d67d7645a51c33dd4b03df
Kim Alex <kimtorres@gmail.com>
2022-01-15 19:39:42 -0300

    Criar consumo de API Rick And Morty

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const imagem1 = document.querySelector('img1');
-const imagem2 = document.querySelector('img2');
-const imagem3 = document.querySelector('img3');
+const imagem1 = document.querySelector('#img1');
+const imagem2 = document.querySelector('#img2');
+const imagem3 = document.querySelector('#img3');
 const botao = document.querySelector('button');
 const nomeDoPersonagem1 = document.querySelector('#nome1');
 const especie1 = document.querySelector('#especie1');
@@ -11,3 +11,59 @@ const condicao2 = document.querySelector('#condicao2');
 const nomeDoPersonagem3 = document.querySelector('#nome3');
 const especie3 = document.querySelector('#especie3');
 const condicao3 = document.querySelector('#condicao3');
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem1.src = data.image;
+        imagem1.alt = data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        condicao1.innerHTML = data.status;
+    });
+}
+
+pegarPersonagem2 = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/1`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML = data.status;
+    });
+}
+
+pegarPersonagem3 = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/3`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem3.src = data.image;
+        imagem3.alt = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML = data.status;
+    });
+}
+
+usarBotao = () => {
+    pegarPersonagem();
+    pegarPersonagem2();
+    pegarPersonagem3();
+}
+
+botao.onclick = usarBotao;
